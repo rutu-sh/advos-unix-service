221ebe6b6eb7e87d5f13c8c9993241c0b98e6aed
prince-noah-johnson <princenoha.j@gmail.com>
2025-03-05 20:24:53 -0500

    Update main server with auth

---
+++ src/server/auth.h
@@ -0,0 +1,25 @@
+#ifndef AUTH_H
+#define AUTH_H
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <sys/resource.h>
+
+#define INVALID_UID ((uid_t)-1U)   
+#define ALLOWED_UID 1000 
+
+struct ucred {  
+    pid_t pid;
+    uid_t uid;
+    gid_t gid;
+};
+
+struct ucred get_client_credentials(int client_fd);
+int is_client_authorized(int client_fd);
+void track_client_resources(int client_fd);
+void print_client_usage(int client_fd);
+
+#endif // AUTH_H
